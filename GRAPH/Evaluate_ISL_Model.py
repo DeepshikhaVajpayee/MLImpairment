1aa3cdc3310790d241675569a594effa1cdeb5c4
DeepshikhaVajpayee <168768963+DeepshikhaVajpayee@users.noreply.github.com>
2026-02-03 11:49:01 +0530

    Add script to visualize model training history
    This script loads training history from a pickle file and generates plots for model accuracy and loss over epochs, saving the loss plot as a PDF.

---
+++ GRAPH/Evaluate_ISL_Model.py
@@ -0,0 +1,24 @@
+import pickle
+import matplotlib.pyplot as plt
+
+with open('history.pkl', 'rb') as f:
+    history = pickle.load(f)
+
+# Plot accuracy
+plt.plot(history['accuracy'])
+plt.plot(history['val_accuracy'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+
+# Plot loss
+plt.plot(history['loss'])
+plt.plot(history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.savefig('mobilenet_loss.pdf', dpi=300)
+plt.show()
